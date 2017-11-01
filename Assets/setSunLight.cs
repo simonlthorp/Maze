cbcce5591c6c4458148950baec3c66d3860f41a2
Krunkery <simonlthorp@gmail.com>
2017-11-01 00:37:45 -0700

    day-night commit

---
+++ Assets/setSunLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setSunLight : MonoBehaviour {
+
+    Material sky;
+
+    public Transform stars;
+    //public Transform worldProbe;
+
+    // Use this for initialization
+    void Start () {
+
+        sky = RenderSettings.skybox;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        stars.transform.rotation = transform.rotation;
+
+        Vector3 tvec = Camera.main.transform.position;
+        //worldProbe.transform.position = tvec;
+
+    }
+}
---
+++ Assets/temp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetSunLight : MonoBehaviour
+{
+
+    Material sky;
+
+    public Transform stars;
+    public Transform worldProbe;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        sky = RenderSettings.skybox;
+
+    }
+
+    bool lighton = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        stars.transform.rotation = transform.rotation;
+
+        
+
+        Vector3 tvec = Camera.main.transform.position;
+        worldProbe.transform.position = tvec;
+
+    }
+}
